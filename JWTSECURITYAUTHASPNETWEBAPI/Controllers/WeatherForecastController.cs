7f2fff62e3ecf4dbb385ec602b257324b2dc08f6
HARI NARAYAN SHUKLA <hari7706shukla@gmail.com>
2023-07-26 16:03:29 +0530

    refactor Weather resources

--- JWTSECURITYAUTHASPNETWEBAPI/Controllers/WeatherForecastController.cs
+++ JWTSECURITYAUTHASPNETWEBAPI/Controllers/WeatherForecastController.cs
@@ -11,23 +11,12 @@ namespace JWTSECURITYAUTHASPNETWEBAPI.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+       
+        [HttpGet]
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public IActionResult Get()
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
